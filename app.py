42d1a8c2584689eec12715cb912bc5c6d5a99853
Muhammad Talha Iqbal kousar <talhajaffar2003@gmail.com>
2024-11-09 13:55:11 +0000

    Get Flask

--- app.py
+++ app.py
@@ -1,2 +1,11 @@
 #Get Flask
-from flask import flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for, session
+
+import secrets
+app = Flask(__name__)
+
+app.secret_key = secrets.token_hex(16)
+
+@app.route("/")
+def index():
+    return render_template("homepage.html")
