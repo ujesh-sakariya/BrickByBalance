f078ac1f4ed7d8216698370de666055a6af16d6b
Ujesh Sakariya <ujesh.sakariya@student.manchester.ac.uk>
2024-11-09 18:29:07 +0000

    up to date;

--- app.py
+++ app.py
@@ -9,3 +9,12 @@ app.secret_key = secrets.token_hex(16)
 @app.route("/")
 def index():
     return render_template("homepage.html")
+
+# handles the mortgage calculator route 
+@app.route('/mortgageCalculator',methods=["GET","POST"])
+def mortgageCalculator():
+
+    if request.method == 'GET':
+        # return the page 
+        return render_template('mortgageCalculator.html')
+
